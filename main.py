ad0128132fe2eda7becb94f21fbf5a6ca610eddc
vaikshnarys <dima.vaikshnarys@gmail.com>
2023-08-20 21:03:08 +0200

    add

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("HelloWorld")
+class Meta(type):
+    def __new__(cls, name,base,attrs):
+        attrs.update({"Solid":1,"Kovid":2})
+        return type.__new__(cls,name,base,attrs)
+class Point(metaclass=Meta):
+    def get_data(self):
+        return (0,0)
+
+pt = Point()
+print(pt.Solid)
